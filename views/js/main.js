b12ae7d9ab536a05033454ee9b83c2675e0f0744
Purvi <purvi194@gmail.com>
2016-11-22 20:20:25 +0530

    Removed recaclculation problem

--- views/js/main.js
+++ views/js/main.js
@@ -501,9 +501,10 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
+  var items = document.getElementsByClassName('mover');
+  vat scrollDoc = (document.body.scrollTop / 1250) ;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin(scrollDoc + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
