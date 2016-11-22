bc25366d098cd350528cb51f6ccab6a40b2c986b
Purvi <purvi194@gmail.com>
2016-11-22 20:13:52 +0530

    reverted more chnages

--- views/js/main.js
+++ views/js/main.js
@@ -507,8 +507,8 @@ function updatePositions() {
 
   var items = document.getElementsByClassName('mover');
   var scrolltop= (document.body.scrollTop / 1250);
-  var phase = Math.sin(scrolltop + (i % 5));
-  for (var i = 0; i < items.length; i++) {    
+  for (var i = 0; i < items.length; i++) {
+    var phase = Math.sin(scrolltop + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
@@ -522,8 +522,6 @@ function updatePositions() {
   }
 }
 
-requestAnimationFrame(updatePositions);
-
 // runs updatePositions on scroll
 window.addEventListener('scroll', updatePositions);
 
@@ -531,7 +529,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 25; i++) {
+  for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
@@ -539,7 +537,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.getElementById("movingPizzas1").appendChild(elem);
+    document.querySelector("#movingPizzas1").appendChild(elem);
   }
   updatePositions();
 });
